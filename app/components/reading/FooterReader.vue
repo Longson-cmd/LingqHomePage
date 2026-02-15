59af3e2ab0e4e04800cc9eedae3e2191f5a73d48
Longson <Longson179159@gmail.com>
2026-02-15 14:27:17 +0700

    add footer reader again

---
+++ app/components/reading/FooterReader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class=" relative flex justify-between items-start  px-3 ">
+    <button @click="openAudioBox = true" :class="openAudioBox && 'invisible pointer-events-none'" class="h-12 w-12 mb-5 rounded-full bg-black/80 flex items-center justify-center">
+        <img src="/icons/reader/play.svg" alt="play"/>
+    </button>
+
+    <div v-if="openAudioBox " class="absolute -top-8 left-5 z-10  bg-white">
+        <AudioYoutubeBox v-if="haveVideoId" @close-audio-box="openAudioBox = $send" :video-id="videoId"/>
+        <AudioBox v-else @close-audio-box="openAudioBox = $send" :audioURL="audioURL"/>
+    </div>
+    
+
+    <button @click="isSentenceView = !isSentenceView; console.log('openAudioBox ', openAudioBox)" class="hover:bg-gray-300 h-8 w-12 rounded-md flex items-end justify-center">
+        <img  :src="isSentenceView ? '/icons/reader/sentenceView.svg' : '/icons/reader/lessonView.svg'" alt="viewMode">
+    </button>
+
+    <NuxtLink :to="isSentenceView? '/gameSentence' : '/gameLesson'"  class="hover:bg-gray-300 h-10 w-12 rounded-md flex items-end justify-center">
+        <img :src="isSentenceView ? '/icons/reader/reviewSentence.svg' : '/icons/reader/reviewLesson.svg'" alt="reviewMode">
+    </NuxtLink>
+
+
+  </div>
+</template>
+
+
+<script setup>
+import {ref, onMounted, onBeforeUnmount} from 'vue'
+
+import AudioBox from './component/AudioBox.vue';
+import AudioYoutubeBox from './component/AudioYoutubeBox.vue';
+const openAudioBox = ref(false)
+const isSentenceView = ref(false)
+
+const haveVideoId = true
+const audioURL = ref('http://localhost:3000/sounds/demo.mp3')
+const videoId = 'sRXnme3pD9M'
+
+
+</script>
