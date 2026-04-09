a9d0016757c6d125b002979e5f87ce9ecfe46e48
Longson <Longson179159@gmail.com>
2026-04-09 10:32:42 +0700

    update new code

--- app/components/reading/FooterReader.vue
+++ app/components/reading/FooterReader.vue
@@ -1,22 +1,22 @@
 <template>
   <div class=" relative flex justify-between items-start  px-3 ">
-    <button @click="openAudioBox = true" :class="openAudioBox && 'invisible pointer-events-none'" class="h-12 w-12 mb-5 rounded-full bg-black/80 flex items-center justify-center">
+    <button @click="openAudioBox = true;" :class="(openAudioBox && props.youtubeData?.youtube_id) && 'invisible pointer-events-none'" class="h-12 w-12 mb-5 rounded-full bg-black/80 flex items-center justify-center">
         <img src="/icons/reader/play.svg" alt="play"/>
     </button>
 
-    <div v-if="openAudioBox " class="absolute -top-8 left-5 z-10  bg-white">
+    <div v-if="openAudioBox && props.youtubeData?.youtube_id" class="absolute -top-8 left-5 z-10  bg-white">
         <AudioYoutubeBox 
-        @close-audio-box="openAudioBox = $send" 
+        @close-audio-box="openAudioBox = $event" 
         :youtube-data="props.youtubeData"/>
         <!-- <AudioBox v-else @close-audio-box="openAudioBox = $send" :audioURL="audioURL"/> -->
     </div>
     
 
-    <button @click="isSentenceView = !isSentenceView; console.log('openAudioBox ', openAudioBox)" class="hover:bg-gray-300 h-8 w-12 rounded-md flex items-end justify-center">
+    <button @click="isSentenceView = !isSentenceView" class="hover:bg-gray-300 mt-4 h-8 w-12 rounded-md flex items-center justify-center">
         <img  :src="isSentenceView ? '/icons/reader/sentenceView.svg' : '/icons/reader/lessonView.svg'" alt="viewMode">
     </button>
 
-    <NuxtLink :to="isSentenceView? '/gameSentence' : '/gameLesson'"  class="hover:bg-gray-300 h-10 w-12 rounded-md flex items-end justify-center">
+    <NuxtLink  class="hover:bg-gray-300 h-10 w-12 rounded-md flex items-end justify-center">
         <img :src="isSentenceView ? '/icons/reader/reviewSentence.svg' : '/icons/reader/reviewLesson.svg'" alt="reviewMode">
     </NuxtLink>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
 </template> -->
 
 <template>
-  <div class="h-screen flex items-center justify-center px-5">
+  <div v-if="isLargeScreen" class="h-screen flex items-center justify-center px-5">
     <div class="w-full max-w-[400px] border rounded-xl p-5 flex flex-col gap-3 shadow">
       <h2 class="text-center text-2xl font-bold pb-5">Welcome back</h2>
 
@@ -43,11 +43,25 @@
       </span>
     </div>
   </div>
+  <!-- GẠCH CHÂN DÒNG NÀY -->
+  <div v-else class="h-screen text-xl text-blue-600 font-bold flex items-center justify-center underline px-5 text-center">
+    <h2>This app temporarily only supports large screens !</h2>
+  </div>
+
+
 </template>
 
 <script setup>
 
 import { ref } from "vue";
+import { useBreakpoints } from "@vueuse/core";
+
+
+const breakpoints = useBreakpoints({
+  md: 768,
+})
+
+const isLargeScreen = breakpoints.greater("md")
 
 const email = ref("");
 const password = ref("");
@@ -87,3 +101,8 @@ const handleSubmit = async () => {
 <!-- sudo ss -lntp | grep ':3000'
 kill 225842 -->
 <!-- nohup npm run dev -- --host 0.0.0.0 --port 3000 > ~/frontend.log 2>&1 & -->
+
+
+<!-- $env:NUXT_API_PROXY_BASE="http://3.26.146.123"
+$env:NUXT_PUBLIC_API_BASE="/api"
+npm run dev -->
